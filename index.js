93c1acd1b3efd314c7847455832491ba03021f1f
cealnollins <134967758+cealnollins@users.noreply.github.com>
2023-08-04 11:52:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function calc () {
     let ninCnt = parseInt(document.getElementById("ninCnt").value);
     let teeCnt = parseInt(document.getElementById("teeCnt").value);
     let vlvCnt = parseInt(document.getElementById("vlvCnt").value);
-    let fitMth = (ninCnt + teeCnt) * 2 + (vlvCnt * 3 + pipeFt);
+    let fitMth = (ninCnt + teeCnt) * 2 + (vlvCnt * 3) + pipeFt;
     let wasteMath = (fitMth *1.05);
     let cblCst = parseFloat(document.getElementById("cblCst").value) * wasteMath;
     let cblPerTherm = parseFloat(document.getElementById("cblPerTherm").value);
