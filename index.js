0f255fd00ef926e1a6dfa003fa029bcdea22b519
cealnollins <134967758+cealnollins@users.noreply.github.com>
2023-08-04 11:08:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ function calc () {
     let ninCnt = parseInt(document.getElementById("ninCnt").value);
     let teeCnt = parseInt(document.getElementById("teeCnt").value);
     let vlvCnt = parseInt(document.getElementById("vlvCnt").value);
-    let endSealPerBag = parseInt(document.getElementById("endSealPerBag").value);
-    let cstPerEnd = parseInt(document.getElementById("cstPerEnd").value);
     let fitMth = (ninCnt + teeCnt) * 2 + (vlvCnt * 3 + pipeFt);
     let wasteMath = (fitMth *1.05);
     let cblCst = parseFloat(document.getElementById("cblCst").value) * wasteMath;
     let cblPerTherm = parseFloat(document.getElementById("cblPerTherm").value);
     let amntTherm = wasteMath / cblPerTherm;
-    let endCnt = (amntTherm + teeCnt) / 5;
     let thermCst = parseFloat(document.getElementById("thermCst").value) * amntTherm;
     let finalMath = thermCst + cblCst;
-    console.log(endCnt);
+    console.log(amntTherm + teeCnt);
     let res = document.getElementById("result").innerHTML="Your heat trace cost is: " + "$" + finalMath;   
 }
 
