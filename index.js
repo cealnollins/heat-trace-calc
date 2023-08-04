154db8dee326ef09985524880d54ecc81f6fddbc
cealnollins <134967758+cealnollins@users.noreply.github.com>
2023-08-04 14:02:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function calc () {
     let tBagCst = (teeCnt / 5) * cstPerTBag
     let lbrCst = (wasteMath / cblPerDay) * lbrPerDay
     let finalMath = thermCst + cblCst + endCst + tapeCst + strapCst + tBagCst + lbrCst ;
-    let res = document.getElementById("result").innerHTML="Your heat trace cost is: " + "$" + finalMath *1.35;   
+    let res = document.getElementById("result").innerHTML="Your heat trace cost is: " + "$" + Math.ceil(finalMath *1.35);   
 }
 
 btnEl.addEventListener("click", calc)
